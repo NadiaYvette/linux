5de1a073b6be148a6aeeb93abb97ff2e10e231d0
Nadia Yvette Chambers <nadia.yvette.chambers@ik.me>
2026-06-27 01:55:35 +0200

    riscv: support page clustering (PAGE_MMUSHIFT)

--- arch/riscv/kernel/patch.c
+++ arch/riscv/kernel/patch.c
@@ -42,20 +42,26 @@ static inline bool is_kernel_exittext(uintptr_t addr)
 static __always_inline void *patch_map(void *addr, const unsigned int fixmap)
 {
 	uintptr_t uintaddr = (uintptr_t) addr;
-	phys_addr_t phys;
+	phys_addr_t pa;
 
 	if (core_kernel_text(uintaddr) || is_kernel_exittext(uintaddr)) {
-		phys = __pa_symbol(addr);
+		/*
+		 * Use __pa_symbol() directly instead of the phys_to_page() /
+		 * page_to_phys() roundtrip.  The latter requires a populated
+		 * sparsemem section map, which is not yet available when
+		 * apply_boot_alternatives() runs from setup_arch().
+		 */
+		pa = __pa_symbol(addr);
 	} else if (IS_ENABLED(CONFIG_STRICT_MODULE_RWX)) {
 		struct page *page = vmalloc_to_page(addr);
 
 		BUG_ON(!page);
-		phys = page_to_phys(page) + offset_in_page(addr);
+		pa = page_to_phys(page) + offset_in_page(addr);
 	} else {
 		return addr;
 	}
 
-	return (void *)set_fixmap_offset(fixmap, phys);
+	return (void *)set_fixmap_offset(fixmap, pa);
 }
 
 static void patch_unmap(int fixmap)
@@ -66,13 +72,13 @@ NOKPROBE_SYMBOL(patch_unmap);
 
 static int __patch_insn_set(void *addr, u8 c, size_t len)
 {
-	bool across_pages = (offset_in_page(addr) + len) > PAGE_SIZE;
+	bool across_pages = (((uintptr_t)addr & ~MMUPAGE_MASK) + len) > MMUPAGE_SIZE;
 	void *waddr = addr;
 
 	/*
 	 * Only two pages can be mapped at a time for writing.
 	 */
-	if (len + offset_in_page(addr) > 2 * PAGE_SIZE)
+	if (len + ((uintptr_t)addr & ~MMUPAGE_MASK) > 2 * MMUPAGE_SIZE)
 		return -EINVAL;
 	/*
 	 * Before reaching here, it was expected to lock the text_mutex
@@ -84,7 +90,7 @@ static int __patch_insn_set(void *addr, u8 c, size_t len)
 	preempt_disable();
 
 	if (across_pages)
-		patch_map(addr + PAGE_SIZE, FIX_TEXT_POKE1);
+		patch_map(addr + MMUPAGE_SIZE, FIX_TEXT_POKE1);
 
 	waddr = patch_map(addr, FIX_TEXT_POKE0);
 
@@ -111,14 +117,14 @@ NOKPROBE_SYMBOL(__patch_insn_set);
 
 static int __patch_insn_write(void *addr, const void *insn, size_t len)
 {
-	bool across_pages = (offset_in_page(addr) + len) > PAGE_SIZE;
+	bool across_pages = (((uintptr_t)addr & ~MMUPAGE_MASK) + len) > MMUPAGE_SIZE;
 	void *waddr = addr;
 	int ret;
 
 	/*
 	 * Only two pages can be mapped at a time for writing.
 	 */
-	if (len + offset_in_page(addr) > 2 * PAGE_SIZE)
+	if (len + ((uintptr_t)addr & ~MMUPAGE_MASK) > 2 * MMUPAGE_SIZE)
 		return -EINVAL;
 
 	/*
@@ -138,7 +144,7 @@ static int __patch_insn_write(void *addr, const void *insn, size_t len)
 	preempt_disable();
 
 	if (across_pages)
-		patch_map(addr + PAGE_SIZE, FIX_TEXT_POKE1);
+		patch_map(addr + MMUPAGE_SIZE, FIX_TEXT_POKE1);
 
 	waddr = patch_map(addr, FIX_TEXT_POKE0);
 
@@ -188,7 +194,7 @@ static int patch_insn_set(void *addr, u8 c, size_t len)
 	 * loop with len <= 2 * PAGE_SIZE.
 	 */
 	while (len) {
-		size = min(len, PAGE_SIZE * 2 - offset_in_page(addr));
+		size = min(len, MMUPAGE_SIZE * 2 - ((uintptr_t)addr & ~MMUPAGE_MASK));
 		ret = __patch_insn_set(addr, c, size);
 		if (ret)
 			return ret;
@@ -223,7 +229,7 @@ int patch_insn_write(void *addr, const void *insn, size_t len)
 	 * because __patch_insn_write() can only handle len <= 2 * PAGE_SIZE.
 	 */
 	while (len) {
-		size = min(len, PAGE_SIZE * 2 - offset_in_page(addr));
+		size = min(len, MMUPAGE_SIZE * 2 - ((uintptr_t)addr & ~MMUPAGE_MASK));
 		ret = __patch_insn_write(addr, insn, size);
 		if (ret)
 			return ret;
